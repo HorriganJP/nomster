b9bf702e76e0a979f246ce09eedf36b15a63d06e
JP Horrigan <Horrigan.JP@outlook.com>
2019-06-08 17:04:19 +0000

    Lession 21 Complete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
+    @place = current_user.places.create(place_params)
     redirect_to root_path
+  end
+
+  def show
+    @place = Place.find(params[:id])
   end
 
   private
