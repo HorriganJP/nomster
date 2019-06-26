b23c78b9c1151fb1d3202e1bd0717e227fdc5955
JP Horrigan <Horrigan.JP@outlook.com>
2019-06-26 00:26:07 +0000

    Lession 39, show.html.erb file not displaying phot?

--- db/migrate/20190625004431_create_captions.rb
+++
@@ -1,6 +0,0 @@
-class CreateCaptions < ActiveRecord::Migration[5.2]
-  def change
-    create_table :captions do |t|
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_25_013519) do
+ActiveRecord::Schema.define(version: 2019_06_26_001423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "captions", force: :cascade do |t|
-  end
 
   create_table "comments", force: :cascade do |t|
     t.text "message"
@@ -30,6 +27,14 @@ ActiveRecord::Schema.define(version: 2019_06_25_013519) do
   end
 
   create_table "photos", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "place_id"
+    t.text "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "picture"
+    t.index ["place_id"], name: "index_photos_on_place_id"
+    t.index ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id"
   end
 
   create_table "places", force: :cascade do |t|
@@ -52,9 +57,6 @@ ActiveRecord::Schema.define(version: 2019_06_25_013519) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "avatar"
-    t.json "avatars"
-    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
