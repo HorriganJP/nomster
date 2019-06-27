dacd13d22ad217437713cd659dda04841ae08745
JP Horrigan <Horrigan.JP@outlook.com>
2019-06-27 19:45:03 +0000

    Lession 43 Complete, 39 NOT PUSHED TO HEROKU

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'no-reply@nomsterapp.com'
+  default from: 'devHorrigan@gmail.com'
   layout 'mailer'
 
-  def comment_added
-  	mail(to: "devHorrigan@gmail.com", subject: "A comment has been added to your place")
+  def comment_added(comment)
+  	@place = comment.place
+  	@place_owner = @place.user
+
+  	mail(to: @place_owner.email, subject: "A comment has been added to #{@place.name}")
   end
 end
