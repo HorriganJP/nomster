a0ca6f5eca2638ee5a02206561ee43c7663d2372
JP Horrigan <Horrigan.JP@outlook.com>
2019-07-01 01:03:03 +0000

    Heroku Complete Published

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,9 @@ CarrierWave.configure do |config|
     provider: 'AWS',                  #REQUIRED
     aws_access_key_id: ENV["AWS_ACCESS_KEY"], #REQUIRED
     aws_secret_access_key: ENV["AWS_SECRET_KEY"], #REQUIRED
+
   }
   config.fog_directory = ENV["AWS_BUCKET"]      #REQUIRED
+  config.fog_public = true
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
 end
